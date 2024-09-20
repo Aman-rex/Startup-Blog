c714aa030ff9bf1b0ce1b9bb0d7410f7e0bc34c8
Aman Pratap Singh <15singh.aman@gmail.com>
2024-09-20 16:28:00 +0530

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App data</div>
+    <div className='text-3xl text-red-500'>App data check</div>
   )
 }
